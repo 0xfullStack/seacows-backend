fc5a0f17e1bf9b784f3a0255a73e3f6f12d1c499
Lin <linshizai.me@gmail.com>
2023-12-01 18:13:49 +0800

    chore: missed commit

--- apps/metadata-service-api/src/server.ts
+++ apps/metadata-service-api/src/server.ts
@@ -1,36 +1,15 @@
 import { initApp } from "./app";
 import logger from "./utils/logger";
-import { AppEnv, getAppEnv } from "./env";
-import external from "src/services";
-import CollectionService from "src/api/collections/collection.service";
+import { getAppEnv } from "./env";
 import { initMoralis } from "./services/moralis";
 
 async function main() {
   const port = process.env.PORT || 3001;
   const env = getAppEnv();
-
   await initMoralis(env.MORALIS_API_KEY);
 
   const app = initApp();
-
-  // test
   app.listen(port, () => logger.log(`Example app listening at http://localhost:${port}`));
-
-  // Reservoir APIs
-  // const collections = await external.reservoirApi.requestMultipleCollections("30DayVolume", 1);
-
-  // for await (const collection of (collections.collections || [])) {
-  //   const address = collection.id;
-
-  //   console.log('Get collection', collection)
-
-  //   if (!address) {
-  //     console.log('Collection address empty', collection)
-  //     continue;
-  //   }
-
-  //   await CollectionService.getCollectionTokens(address);
-  // }
 }
 
 main().catch((error: Error) => {
